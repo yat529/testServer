03c7444e6d84bc16f9b30d35e792914ef1616a63
YaoTonq <yat529@gmail.com>
2019-02-03 01:37:00 -0500

    res index

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + 'public/index.html'))
 })
 
 
