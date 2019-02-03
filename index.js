6442dce886bec39c1e76f1212eb55290d2b70093
YaoTonq <yat529@gmail.com>
2019-02-02 19:17:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(5000, () => console.log('App is running at port 5000!'))
