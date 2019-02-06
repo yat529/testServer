055dea7b0b3a8469acacc5dcdd102f27a0e52e43
YaoTonq <yat529@gmail.com>
2019-02-06 00:04:56 -0500

    add pwa

---
+++ dist/precache-manifest.0ef67d31db2780649e320901524161eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c261d872674fbd189751",
+    "url": "/js/chunk-vendors.77438408.js"
+  },
+  {
+    "revision": "89795d3a3a2a1897258b",
+    "url": "/js/app.c364d3b4.js"
+  },
+  {
+    "revision": "88f9182445292286b4581ec1027d7772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89795d3a3a2a1897258b",
+    "url": "/css/app.9225f659.css"
+  }
+];
